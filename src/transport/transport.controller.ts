2b1b6d86f9e8ebc6a2ab28b77c936ca772c89ceb
Maksim Maliukevich <maksim.maliukevich@intexsoft.by>
2019-05-11 01:30:28 +0300

    feat(transport-module): refactor transport module

--- src/transport/transport.controller.ts
+++ src/transport/transport.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { ApiOperation, ApiCreatedResponse, ApiUseTags } from '@nestjs/swagger';
+import { CreateTransportDto } from './interfaces';
 
-@Controller('transport')
-export class TransportController {}
+@ApiUseTags('api/transport')
+@Controller('api/transport')
+export class TransportController {
+
+    @ApiOperation({ title: 'Adding new transport' })
+    @ApiCreatedResponse({ description: 'Created new transport' })
+    @Post('')
+    async createNewTransport(@Body() transport: CreateTransportDto) {
+        console.log('new transport added to database')
+    }
+
+    @ApiOperation({ title: 'Fetch all transport' })
+    @ApiCreatedResponse({ description: 'Fetch all transport' })
+    @Get('')
+    async getAllTransports() {
+        console.log('fetched all transport')
+    }
+}
